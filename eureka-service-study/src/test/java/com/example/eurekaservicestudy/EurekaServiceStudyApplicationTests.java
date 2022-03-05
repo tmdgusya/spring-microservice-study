a1879ae57ef194110c34bdff0208efbb69de2831
정승현 <roach@woowahan.com>
2022-03-05 22:15:48 +0900

    init

---
+++ eureka-service-study/src/test/java/com/example/eurekaservicestudy/EurekaServiceStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaservicestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
