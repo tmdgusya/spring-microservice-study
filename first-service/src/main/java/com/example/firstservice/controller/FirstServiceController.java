c592afdc3ab7477b16b9252c7b7e4f40e8d7811a
tmdgusya <tmdgusya@naver.com>
2022-04-07 19:09:53 +0900

    Spring GateWay Filter 를 통해 붙은 Header 값 확인

--- first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
@@ -1,6 +1,7 @@
 package com.example.firstservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstServiceController {
 
     @GetMapping("/welcome")
-    public String welcome() {
+    public String welcome(@RequestHeader("first-request") String header) {
+        System.out.println(header);
         return "Welcome to the First Service";
     }
 
--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,6 +1,7 @@
 package com.example.secondservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecondServiceController {
 
     @GetMapping("/welcome")
-    public String welcome() {
+    public String welcome(@RequestHeader("second-request") String header) {
+        System.out.println(header);
         return "Welcome to the Second Service";
     }
 
