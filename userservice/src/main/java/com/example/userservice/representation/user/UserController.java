28c9f2760a4136045d5f99416f060d11a9fac100
tmdgusya <tmdgusya@naver.com>
2022-04-27 21:13:34 +0900

    Sleuth Test

--- userservice/src/main/java/com/example/userservice/representation/user/UserController.java
+++ userservice/src/main/java/com/example/userservice/representation/user/UserController.java
@@ -5,8 +5,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class UserController {
+
+    @PostMapping("/hello")
+    public String hello(HttpServletRequest httpServletRequest) throws InterruptedException {
+        Enumeration<String> headers = httpServletRequest.getHeaders("");
+
+        log.info("zzz = {}", headers.hasMoreElements());
+
+        while (headers.hasMoreElements()) {
+            System.out.println(headers.nextElement());
+        }
+
+        Thread.sleep(3000);
+        return "hello";
+    }
 
     @PostMapping
     public ResponseEntity<Void> registerUser() {
